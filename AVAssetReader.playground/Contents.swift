593bfe5bfec110c7b71bb773229792feb95ba45d
Justin Winter <justinlevi@gmail.com>
2015-08-23 20:26:04 -0400

    reduce working

--- AVAssetReader.playground/Contents.swift
+++ AVAssetReader.playground/Contents.swift
@@ -20,104 +20,50 @@ class SSSampleDataFilter {
   func filteredSamplesForSize(size:CGSize) -> [Int16]{
     var filterSamples = [Int16]()
     
-    size
-    
     if let sampleData = sampleData {
-      var sampleCount = sampleData.length
+      let sampleCount = sampleData.length
       
       var binSize = Int(CGFloat(sampleCount) / size.width)
       binSize = binSize > 2 ? binSize : 2
-      
-      // PRETTY SURE THE BINSIZE MUST BE A MULTIPLE OF 2 AS WELL
-      
-      // TEMPORARY
-      //binSize = sampleCount
+      binSize = binSize % 2 == 0 ? binSize : binSize + 1 // BINSIZE MUST BE A MULTIPLE OF 2
       
       let stream = NSInputStream(data: sampleData)
       stream.open()
       
-      var readBuffer = Array<UInt8>(count: 1024 * 8, repeatedValue: 0)
+      var readBuffer = Array<UInt8>(count: 1024 * 32, repeatedValue: 0)
+      
       var totalBytesRead = 0
-      
-      
-      // TEMPORARY
-//      sampleCount = 5
-      
-//      let size = sizeof(Int16)
-
-  
-//      while (totalBytesRead < sampleCount) {
-//        let numberOfBytesRead = stream.read(&readBuffer, maxLength: size)
-//        let u16: Int16 = UnsafePointer<Int16>(readBuffer).memory
-//        
-//        var sampleBin = [Int16]()
-//        for _ in 0..<Int(binSize) {
-//          sampleBin.append(u16)
-//        }
-//        
-//        filterSamples.append(sampleBin.maxElement()!)
-//        totalBytesRead += numberOfBytesRead
-//      }
-      
-      
       while (totalBytesRead < sampleCount) {
         let numberOfBytesRead = stream.read(&readBuffer, maxLength: readBuffer.count)
-  
         
-        // TEMPORARY
-        // var sampleBin = [Int16]()
-      
-        // This returns the same value as reading the stream 2 bytes at a time
-        // var tmpBuffer = Array<UInt8>(arrayLiteral: readBuffer[2], readBuffer[3])
-        // let u16: Int16 = UnsafePointer<Int16>(tmpBuffer).memory
-        
-        // for var i = 0; i < numberOfBytesRead; i+=2 {
-          
-        // END - TEMPORARY
-        
+        // Don't be lured into fancy functional solutions for this as it's probably slower
+        // per stack overflow post here http://stackoverflow.com/questions/28929804
         for var i=0; i < numberOfBytesRead; i+=binSize {
-          
           var sampleBin = [Int16]()
           
-          // TEMPORARY
-          // if i + 1 < readBuffer.count {
-          //  var tmpBuffer = Array<UInt8>(arrayLiteral: readBuffer[i], readBuffer[i+1])
-          //  let u16: Int16 = UnsafePointer<Int16>(tmpBuffer).memory
-          //  sampleBin.append(u16)
-          // }
-          // END - TEMPORARY
-          
           for var j = 0; j < binSize; j+=2 {
-    
             if i+j+1 < readBuffer.count {
               let tmpBuffer = Array<UInt8>(arrayLiteral: readBuffer[i+j], readBuffer[i+j+1])
               let u16: Int16 = UnsafePointer<Int16>(tmpBuffer).memory
               sampleBin.append(u16)
-              
-              //filterSamples.append(u16)
             }
           }
           
           filterSamples.append(sampleBin.maxElement()!)
-          
         }
 
         totalBytesRead += numberOfBytesRead
       }
     
       plotArrayInPlayground(filterSamples, title: "Samples")
-  
     }
     
-    
-    
     return filterSamples
-    
   }
 }
 
 // MP3 doesn't work
-let sineURL = NSBundle.mainBundle().URLForResource("440.0-saw-0.05s", withExtension: "aif")!
+let sineURL = NSBundle.mainBundle().URLForResource("440.0-sine", withExtension: "mp3")!
 let asset = AVAsset(URL: sineURL)
 var assetReader:AVAssetReader
 
